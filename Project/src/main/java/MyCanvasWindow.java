e43f366ca422bba34e748fab140df506c1ac82f9
Wouter <woutervmertens@gmail.com>
2020-02-29 17:36:57 +0100

    bewegend balkje

--- Project/src/main/java/MyCanvasWindow.java
+++ Project/src/main/java/MyCanvasWindow.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class MyCanvasWindow extends CanvasWindow {
 
         /**
@@ -8,5 +10,25 @@ public class MyCanvasWindow extends CanvasWindow {
         protected MyCanvasWindow(String title) {
             super(title);
         }
+        int x = 0;
+        int add = 1;
 
+    @Override
+    protected void paint(Graphics g) {
+        g.setColor(Color.GRAY);
+        g.fillRect(x,0,60,60);
+        x += add;
+        if(x > 540 || x < 1) add *= -1;
+        repaint();
+    }
+
+    @Override
+    protected void handleMouseEvent(int id, int x, int y, int clickCount) {
+        super.handleMouseEvent(id, x, y, clickCount);
+    }
+
+    @Override
+    protected void handleKeyEvent(int id, int keyCode, char keyChar) {
+        super.handleKeyEvent(id, keyCode, keyChar);
+    }
 }
