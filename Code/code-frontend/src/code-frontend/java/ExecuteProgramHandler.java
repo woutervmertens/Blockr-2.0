cc01968bb60a1b2060db772315435f0e8ea1e239
Wouter <woutervmertens@gmail.com>
2020-03-13 16:29:16 +0100

    add block to backend

--- Code/code-frontend/src/code-frontend/java/ExecuteProgramHandler.java
+++ Code/code-frontend/src/code-frontend/java/ExecuteProgramHandler.java
@@ -1,7 +1,5 @@
-import UIElements.Directions;
+import UIElements.UIBlock;
 import UIElements.UICharacter;
-
-import java.awt.*;
 
 public class ExecuteProgramHandler {
     private UIProgramArea uiProgramArea;
@@ -27,59 +25,8 @@ public class ExecuteProgramHandler {
         uiGameWorld.setGrid(converter.convertGrid(blockrGame.getGrid()));
     }
 
-    public void turnCharacter(Directions direction)
-    {
-        getCharacter();
-        uiCharacter.setDirection(direction);
+    public void addBlockToProgram(UIBlock block){
+        blockrGame.addBlockToBlockGroudp(block.getBlock());
     }
 
-    public void moveCharacter(int x, int y){
-        getCharacter();
-        //uiGameWorld.switchElements(uiCharacter.getPosInGrid(),new Point(x,y));
-    }
-
-    private void getCharacter()
-    {
-        if (uiCharacter == null){
-            uiCharacter = uiGameWorld.getCharacter();
-        }
-    }
-
-    /*public void turnLeft()
-    {
-        getCharacter();
-        switch(uiCharacter.getDirection()){
-            case UP:
-                turnCharacter(Directions.LEFT);
-                break;
-            case RIGHT:
-                turnCharacter(Directions.UP);
-                break;
-            case DOWN:
-                turnCharacter(Directions.RIGHT);
-                break;
-            case LEFT:
-                turnCharacter(Directions.DOWN);
-                break;
-        }
-    }
-
-    public void turnRight()
-    {
-        getCharacter();
-        switch(uiCharacter.getDirection()){
-            case UP:
-                turnCharacter(Directions.RIGHT);
-                break;
-            case RIGHT:
-                turnCharacter(Directions.DOWN);
-                break;
-            case DOWN:
-                turnCharacter(Directions.LEFT);
-                break;
-            case LEFT:
-                turnCharacter(Directions.UP);
-                break;
-        }
-    }*/
 }
