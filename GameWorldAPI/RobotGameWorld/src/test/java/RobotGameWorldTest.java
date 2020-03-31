2d955e1ca4775802f4969518cfa6b9bb235e697a
Wouter <woutervmertens@gmail.com>
2020-03-31 12:58:14 +0200

    generate tests

---
+++ GameWorldAPI/RobotGameWorld/src/test/java/RobotGameWorldTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotGameWorldTest {
+
+    @org.junit.jupiter.api.Test
+    void setSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDrawPosition() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRobot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGrid() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void doAction() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createSnapshot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restoreSnapshot() {
+    }
+}
