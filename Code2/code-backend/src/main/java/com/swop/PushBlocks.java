bf0642adf69c683c422851d4ed6ec4261c5df573
Adam El M'Rabet <e.adam2008@hotmail.fr>
2020-04-22 01:24:32 +0200

    Corrected error of pushing twice

--- Code2/code-backend/src/main/java/com/swop/PushBlocks.java
+++ Code2/code-backend/src/main/java/com/swop/PushBlocks.java
@@ -11,17 +11,17 @@ public interface PushBlocks {
     /**
      * Push down or up (based on the sign of the given distance) all blocks of the given list starting from the given index.
      */
-    static void pushBlocksInListFromIndexWithDistance(List<Block> bodyBlocks, int index, int distance) {
+    static void pushBlocksInListFromIndexWithDistance(List<Block> blockList, int index, int distance) {
         if (index < 0) return;
         // Push all the body blocks starting from the index
-        for (int i = index; i < bodyBlocks.size(); i++) {
-            Block currentBlock = bodyBlocks.get(i);
+        for (int i = index; i < blockList.size(); i++) {
+            Block currentBlock = blockList.get(i);
             currentBlock.setPosition(new Point(currentBlock.getPosition().x, currentBlock.getPosition().y + distance));
-            if (currentBlock instanceof StatementBlock) {
-                for (Block currentBodyBlock : ((StatementBlock) currentBlock).getBodyBlocks()) {
-                    currentBodyBlock.setPosition(new Point(currentBodyBlock.getPosition().x, currentBodyBlock.getPosition().y + distance));
-                }
-            }
+//            if (currentBlock instanceof StatementBlock) {
+//                for (Block currentBodyBlock : ((StatementBlock) currentBlock).getBodyBlocks()) {
+//                    currentBodyBlock.setPosition(new Point(currentBodyBlock.getPosition().x, currentBodyBlock.getPosition().y + distance));
+//                }
+//            }
         }
     }
 
