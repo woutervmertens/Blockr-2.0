0fc08bfd877a7a2e5d373df32850111d2e511dbf
Kristof <kristof.schollen@gmail.com>
2020-04-24 02:01:49 +0200

    fixed test

--- Code2/code-backend/src/test/java/com/swop/command/ExecuteCommandTest.java
+++ Code2/code-backend/src/test/java/com/swop/command/ExecuteCommandTest.java
@@ -17,7 +17,7 @@ class ExecuteCommandTest {
     private Point position = new Point(1,1);
     private int height =2;
     private int width = 3;
-    private Action action = Action.MOVE_FORWARD;
+    private Action action = Action.TURN_LEFT;
     private ActionBlock block = new ActionBlock(position, width, height, action);
 
 
@@ -46,10 +46,10 @@ class ExecuteCommandTest {
     void executeAndUndo() {
         executeCommand.execute();
         RobotSnapshot snapshot1 = (RobotSnapshot) gameWorld.createSnapshot();
-        assertNotEquals(snapshot.getRobot().getPosition(), snapshot1.getRobot().getPosition(), "position of the robot is the same in both snapshots");
+        assertNotEquals(snapshot.getRobot().getDirection(), snapshot1.getRobot().getDirection(), "direction of the robot is the same in both snapshots");
         executeCommand.undo();
         RobotSnapshot snapshot2 = (RobotSnapshot) gameWorld.createSnapshot();
-        assertEquals(snapshot.getRobot().getPosition(), snapshot2.getRobot().getPosition(), "position of the robot isn't the same in both snapshots");
+        assertEquals(snapshot.getRobot().getDirection(), snapshot2.getRobot().getDirection(), "direction of the robot isn't the same in both snapshots");
 
     }
 
