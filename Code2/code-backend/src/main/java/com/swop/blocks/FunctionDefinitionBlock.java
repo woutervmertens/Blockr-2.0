47e0d88bb0049a0acd5d04a808a7da2ac00842a6
Wouter <woutervmertens@gmail.com>
2020-05-19 17:15:46 +0200

    FunctionDefinitionBlock extends BlockWithBody

--- Code2/code-backend/src/main/java/com/swop/blocks/FunctionDefinitionBlock.java
+++ Code2/code-backend/src/main/java/com/swop/blocks/FunctionDefinitionBlock.java
@@ -1,4 +1,24 @@
 package com.swop.blocks;
 
-public class FunctionDefinitionBlock {
+import java.awt.*;
+
+public class FunctionDefinitionBlock extends BlockWithBody{
+    protected FunctionDefinitionBlock(Point position, int width, int height) {
+        super(position, width, height);
+    }
+
+    @Override
+    protected void handleEndOfBody() {
+
+    }
+
+    @Override
+    public Point getSocketPosition() {
+        return null;
+    }
+
+    @Override
+    public Point getPlugPosition() {
+        return null;
+    }
 }
