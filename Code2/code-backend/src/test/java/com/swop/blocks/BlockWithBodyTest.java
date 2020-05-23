19333b944521ae5c2f76b62db6d58f0ef1013e6f
Wouter <woutervmertens@gmail.com>
2020-05-23 15:19:56 +0200

    final blocktests until call and definition are done

--- Code2/code-backend/src/test/java/com/swop/blocks/BlockWithBodyTest.java
+++ Code2/code-backend/src/test/java/com/swop/blocks/BlockWithBodyTest.java
@@ -1,82 +1,147 @@
 package com.swop.blocks;
 
+import com.swop.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BlockWithBodyTest {
+    FunctionDefinitionBlock block;
+    Point position;
+    ActionBlock action, action2;
+    BlockrGame blockrGame;
+
 
     @BeforeEach
     void setUp(){
-
+        position = new Point(10,10);
+        block = new FunctionDefinitionBlock(position,1,1);
+        action = new ActionBlock(position,1,1, RobotAction.TURN_LEFT);
+        action2 = new ActionBlock(position,1,1, RobotAction.TURN_RIGHT);
+        blockrGame = new BlockrGame(10,new RobotGameWorldType());
+        action.setBlockrGame(blockrGame);
     }
 
     @Test
     void setPosition() {
+        block.getBodyBlocks().add(action);
+        block.getBodyBlocks().add(action2);
+        Point newPos = new Point(99,99);
+        block.setPosition(newPos);
+        assertEquals(newPos,block.getPosition());
+        assertEquals(newPos,block.getBodyBlocks().get(0).getPosition());
+        assertEquals(newPos,block.getBodyBlocks().get(1).getPosition());
     }
 
     @Test
     void addBodyBlockAfter() {
+        assertThrows(IllegalArgumentException.class, () -> block.addBodyBlockAfter(action,null));
+        assertThrows(IllegalArgumentException.class, () -> block.addBodyBlockAfter(action,action2));
+        block.getBodyBlocks().add(action);
+        block.addBodyBlockAfter(action2,action);
+        assertEquals(action,block.getBodyBlocks().get(0));
+        assertEquals(action2,block.getBodyBlocks().get(1));
     }
 
     @Test
     void addBodyBlockBefore() {
+        assertThrows(IllegalArgumentException.class, () -> block.addBodyBlockBefore(action,null));
+        assertThrows(IllegalArgumentException.class, () -> block.addBodyBlockBefore(action,action2));
+        block.getBodyBlocks().add(action2);
+        block.addBodyBlockBefore(action,action2);
+        assertEquals(action,block.getBodyBlocks().get(0));
+        assertEquals(action2,block.getBodyBlocks().get(1));
     }
 
     @Test
     void insertBodyBlockAtIndex() {
+        block.insertBodyBlockAtIndex(action2,0);
+        assertEquals(action2,block.getBodyBlocks().get(0));
+        block.insertBodyBlockAtIndex(action,0);
+        assertEquals(action,block.getBodyBlocks().get(0));
     }
 
     @Test
     void removeBodyBlock() {
+        block.getBodyBlocks().add(action);
+        block.removeBodyBlock(action);
+        assertTrue(block.getBodyBlocks().isEmpty());
     }
 
     @Test
     void resetExecution() {
+        block.setBusy(true);
+        block.resetExecution();
+        assertFalse(block.isBusy());
     }
 
     @Test
     void getNextBodyBlock() {
+        assertEquals(null,block.getNextBodyBlock());
     }
 
     @Test
     void setNextBodyBlock() {
-    }
-
-    @Test
-    void testSetNextBodyBlock() {
+        block.insertBodyBlockAtIndex(action,0);
+        block.insertBodyBlockAtIndex(action2,1);
+        assertEquals(null,block.getNextBodyBlock());
+        block.setNextBodyBlock();
+        assertEquals(action,block.getNextBodyBlock());
+        block.setNextBodyBlock();
+        assertEquals(action2,block.getNextBodyBlock());
     }
 
     @Test
     void executeNextBodyBlock() {
-    }
+        block.setBusy(true);
+        block.executeNextBodyBlock();
+        assertFalse(block.isBusy());
 
-    @Test
-    void handleEndOfBody() {
+        block.insertBodyBlockAtIndex(action,0);
+        block.setNextBodyBlock();
+        block.executeNextBodyBlock();
+        assertEquals(Direction.UP,((RobotGameWorld)blockrGame.getGameWorld()).getRobot().getDirection());
+
     }
 
     @Test
     void getBodyBlocks() {
+        assertEquals(block.bodyBlocks,block.getBodyBlocks());
     }
 
     @Test
     void getBodyPlugPosition() {
+        assertEquals(20,block.getBodyPlugPosition().x);
+        assertEquals(11,block.getBodyPlugPosition().y);
     }
 
     @Test
     void getGapSize() {
+        assertEquals(0,block.getGapSize());
     }
 
     @Test
     void setGapSize() {
+        block.setGapSize(7);
+        assertEquals(7,block.getGapSize());
     }
 
     @Test
     void increaseGapSize() {
+        block.setGapSize(1);
+        assertEquals(1,block.getGapSize());
+        block.increaseGapSize(2);
+        assertEquals(3,block.getGapSize());
     }
 
     @Test
     void getCount() {
+        assertEquals(1,block.getCount());
+        block.getBodyBlocks().add(action);
+        assertEquals(2,block.getCount());
     }
 }
--- Code2/code-backend/src/test/java/com/swop/blocks/FunctionCallBlockTest.java
+++ Code2/code-backend/src/test/java/com/swop/blocks/FunctionCallBlockTest.java
@@ -1,10 +1,23 @@
 package com.swop.blocks;
 
+import com.swop.Action;
+import com.swop.BlockrGame;
+import com.swop.RobotAction;
+import com.swop.RobotGameWorldType;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FunctionCallBlockTest {
+    private final Point position = new Point(5, 1);
+    private final int width = 3;
+    private final int height = 4;
+    private final FunctionDefinitionBlock definition = new FunctionDefinitionBlock(position,width,height);
+    private BlockrGame blockrGame = new BlockrGame(10, new RobotGameWorldType());
+
+    private final FunctionCallBlock callBlock = new FunctionCallBlock(position, width, height, definition);
 
     @Test
     void execute() {
@@ -12,9 +25,15 @@ class FunctionCallBlockTest {
 
     @Test
     void getPlugPosition() {
+        Point expectedPosition = new Point(position.x, position.y + callBlock.getHeight() + callBlock.getStep());
+        assertEquals(expectedPosition.x, callBlock.getPlugPosition().x, "Expected x-position and actual x-position of plug are different");
+        assertEquals(expectedPosition.y, callBlock.getPlugPosition().y, "Expected y-position and actual y-position of plug are different");
     }
 
     @Test
     void getSocketPosition() {
+        Point expectedPosition = new Point(position.x, position.y + callBlock.getStep());
+        assertEquals(expectedPosition.x, callBlock.getSocketPosition().x, "Expected x-position and actual x-position of socket are different");
+        assertEquals(expectedPosition.y, callBlock.getSocketPosition().y, "Expected y-position and actual y-position of socket are different");
     }
 }
